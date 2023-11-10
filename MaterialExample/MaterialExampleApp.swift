29822dc217c58c8b7b04f39322eaa02efe10a165
Russell Gordon <rgordon@lcs.on.ca>
2023-11-09 22:21:55 -0500

    Initial Commit

---
+++ MaterialExample/MaterialExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaterialExampleApp.swift
+//  MaterialExample
+//
+//  Created by Russell Gordon on 2023-11-09.
+//
+
+import SwiftUI
+
+@main
+struct MaterialExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
